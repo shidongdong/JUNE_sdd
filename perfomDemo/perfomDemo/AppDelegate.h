7ef922221afc614dc64cbbd63cd94094f0a1b164
shidongdong <975684878@qq.com>
2014-03-05 11:03:28 +0800

    1.0
    第一个工程

---
+++ perfomDemo/perfomDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  perfomDemo
+//
+//  Created by dh on 14-3-3.
+//  Copyright (c) 2014年 public. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ perfomDemo/perfomDemoTests/perfomDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  perfomDemoTests.m
+//  perfomDemoTests
+//
+//  Created by dh on 14-3-3.
+//  Copyright (c) 2014年 public. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface perfomDemoTests : XCTestCase
+
+@end
+
+@implementation perfomDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
